616336d5a69c3c96644dee6b105f6cdd8775c108
swarajns <swarajns79@gmail.com>
2025-10-05 22:03:20 +0530

    hamburger

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,15 +24,6 @@ export default function Header({ currentPage }: HeaderProps) {
           </Link>
           
           <Navigation currentPage={currentPage} />
-          
-          {/* Mobile Menu Button */}
-          <div className="md:hidden">
-            <button className="text-gray-700 hover:text-yellow-600">
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
-            </button>
-          </div>
         </div>
       </div>
     </header>
